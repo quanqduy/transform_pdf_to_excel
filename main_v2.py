fb63d83fb027f074555b1b456a10a956494ca38a
quanqduy <129574439+quanqduy@users.noreply.github.com>
2026-03-18 18:43:19 +0100

    Update main_v2.py

--- main_v2.py
+++ main_v2.py
@@ -1,8 +1,8 @@
 import pandas as pd
 # import numpy as np
 # from datetime import date
-import pdfplumber, os # , sys
-# from tkinter import filedialog
+import pdfplumber, os 
+# import sys
 import tempfile
 
 '''command 17.03.2026'''
@@ -61,6 +61,10 @@ def func_main_process(input_list):
     for tbl in input_list:
         # print(tbl)
         df_readed = function_read_pdf(table_name = tbl)
+
+        #Add 18.03.2026
+        df_readed['FILE_PATH'] = tbl
+
         list_df.append(df_readed)
     tables = pd.concat(list_df, ignore_index=True)
     # print(tables)
@@ -107,41 +111,54 @@ def func_main_process(input_list):
     #     None
 
     # print(tables)
-    '''Add 17.03.2026'''
-    po_delivery_date_df = tables[['MA_DON_HANG', 'DELIVERY_DATE']].drop_duplicates().dropna()
+    '''Add 18.03.2026'''
+    po_delivery_date_df = tables[['MA_DON_HANG', 'DELIVERY_DATE', 'FILE_PATH']].drop_duplicates().dropna()
+    #-------------------
+    # new_row = {'MA_DON_HANG': '359010022025', 'DELIVERY_DATE': '31/07/2025', 'FILE_PATH': 'D:\WORK\Projekte\Prj_Transform pdf_excel\Folder PO\Purchase Order_359010022024_1747119879612.pdf'}
+    # po_delivery_date_df = pd.concat([po_delivery_date_df, pd.DataFrame([new_row])], ignore_index=True)
+    #-------------------
+    po_delivery_date_df = po_delivery_date_df.sort_values(by=['MA_DON_HANG', 'DELIVERY_DATE'], ascending=[True, True], ignore_index=True)
 
-    '''Khởi tạo template kết quả - excel + xử lý dữ liệu khi process xong pdf'''
-    template_excel = pd.DataFrame(columns=['Số PO', 'STT', 'Mã SP 7 số', 'Mã SP 13 số', 'Tên sản phẩm', 'Số lượng', 'Đơn giá', '% Thuế', 'Giá trị sau VAT', 'Hạn sử dụng tối thiểu'])
-    template_excel['Số PO'] = tables['MA_DON_HANG']
-    template_excel['STT'] = template_excel.groupby(['Số PO']).cumcount()
-    # template_excel['Ngày phân phối'] = tables['DELIVERY_DATE']
-    template_excel['Mã SP 7 số'] = tables['CHI TIẾT MÃ HÀNG'].str[:7].str.replace('\n', '', regex=False)
-    template_excel['Mã SP 13 số'] = tables['CHI TIẾT MÃ HÀNG'].str[8:21].str.replace('\n', '', regex=False)
-    template_excel['Tên sản phẩm'] = tables['CHI TIẾT MÃ HÀNG'].str[21:].str.replace('\n', ' ', regex=False)
-    template_excel['Số lượng'] = tables['SL ĐẶT HÀNG']
-    template_excel['Hạn sử dụng tối thiểu'] = tables['HSD TỐI THIỂU']
-    template_excel['Đơn giá'] = tables['ĐƠN GIÁ TỈ LỆ VAT'].str.split('\n').str[0]
-    template_excel['% Thuế'] = tables['ĐƠN GIÁ TỈ LỆ VAT'].str.split('\n').str[1]
-    template_excel['Giá trị sau VAT'] = tables['TỔNG CỘNG GIÁ TRỊ VAT'].str.split('\n').str[0]
+    is_unique = po_delivery_date_df['MA_DON_HANG'].is_unique
+    if not is_unique:
+        duplicates = po_delivery_date_df[po_delivery_date_df.duplicated(subset=['MA_DON_HANG'], keep=False)]
+        # print(duplicates)
+        duplicates.drop(['FILE_PATH'], axis=1, inplace=True)
+        duplicates['REASON'] = 'PO Number is duplicated'
+        template_excel = duplicates
+        flag = False
+    else:
+        # print('Ok')
+        '''Khởi tạo template kết quả - excel + xử lý dữ liệu khi process xong pdf'''
+        template_excel = pd.DataFrame(columns=['Số PO', 'STT', 'Mã SP 7 số', 'Mã SP 13 số', 'Tên sản phẩm', 'Số lượng', 'Đơn giá', '% Thuế', 'Giá trị sau VAT', 'Hạn sử dụng tối thiểu'])
+        template_excel['Số PO'] = tables['MA_DON_HANG']
+        template_excel['STT'] = template_excel.groupby(['Số PO']).cumcount()
+        # template_excel['Ngày phân phối'] = tables['DELIVERY_DATE']
+        template_excel['Mã SP 7 số'] = tables['CHI TIẾT MÃ HÀNG'].str[:7].str.replace('\n', '', regex=False)
+        template_excel['Mã SP 13 số'] = tables['CHI TIẾT MÃ HÀNG'].str[8:21].str.replace('\n', '', regex=False)
+        template_excel['Tên sản phẩm'] = tables['CHI TIẾT MÃ HÀNG'].str[21:].str.replace('\n', ' ', regex=False)
+        template_excel['Số lượng'] = tables['SL ĐẶT HÀNG']
+        template_excel['Hạn sử dụng tối thiểu'] = tables['HSD TỐI THIỂU']
+        template_excel['Đơn giá'] = tables['ĐƠN GIÁ TỈ LỆ VAT'].str.split('\n').str[0]
+        template_excel['% Thuế'] = tables['ĐƠN GIÁ TỈ LỆ VAT'].str.split('\n').str[1]
+        template_excel['Giá trị sau VAT'] = tables['TỔNG CỘNG GIÁ TRỊ VAT'].str.split('\n').str[0]
+        template_excel['Đơn giá'] = template_excel['Đơn giá'].astype(str).str.replace(',', '', regex=False).str.strip()
+        template_excel['Đơn giá'] = pd.to_numeric(template_excel['Đơn giá'], errors='coerce').fillna(0).astype(int)
+        template_excel['Giá trị sau VAT'] = template_excel['Giá trị sau VAT'].astype(str).str.replace(',', '', regex=False).str.strip()
+        template_excel['Giá trị sau VAT'] = pd.to_numeric(template_excel['Giá trị sau VAT'], errors='coerce').fillna(0).astype(int)
+        '''Lọc bỏ các hàng không liên quan'''
+        template_excel['Số lượng'] = pd.to_numeric(template_excel['Số lượng'], errors='coerce')
+        template_excel = template_excel.dropna(subset=['Số lượng'])
+        template_excel = template_excel.astype({'Số lượng': int})
+        template_excel = template_excel.astype({'Số PO': str, 'Mã SP 7 số': str, 'Mã SP 13 số': str, 'Số lượng': 'int64'})
 
-    template_excel['Đơn giá'] = template_excel['Đơn giá'].astype(str).str.replace(',', '', regex=False).str.strip()
-    template_excel['Đơn giá'] = pd.to_numeric(template_excel['Đơn giá'], errors='coerce').fillna(0).astype(int)
-
-    template_excel['Giá trị sau VAT'] = template_excel['Giá trị sau VAT'].astype(str).str.replace(',', '', regex=False).str.strip()
-    template_excel['Giá trị sau VAT'] = pd.to_numeric(template_excel['Giá trị sau VAT'], errors='coerce').fillna(0).astype(int)
-
-    '''Lọc bỏ các hàng không liên quan'''
-    template_excel['Số lượng'] = pd.to_numeric(template_excel['Số lượng'], errors='coerce')
-    template_excel = template_excel.dropna(subset=['Số lượng'])
-    template_excel = template_excel.astype({'Số lượng': int})
-    template_excel = template_excel.astype({'Số PO': str, 'Mã SP 7 số': str, 'Mã SP 13 số': str, 'Số lượng': 'int64'})
-
-    template_excel = template_excel.merge(po_delivery_date_df, how='left', left_on='Số PO', right_on='MA_DON_HANG')
-    template_excel = template_excel.rename(columns={'DELIVERY_DATE': 'Ngày phân phối'})
-    template_excel.drop(['MA_DON_HANG'], axis=1, inplace=True)
-    template_excel = template_excel[['Số PO', 'STT', 'Ngày phân phối', 'Mã SP 7 số', 'Mã SP 13 số', 'Tên sản phẩm', 'Số lượng', 'Đơn giá', '% Thuế', 'Giá trị sau VAT', 'Hạn sử dụng tối thiểu']]
-    print('Created template excel - done')
-    return template_excel
+        template_excel = template_excel.merge(po_delivery_date_df[['MA_DON_HANG', 'DELIVERY_DATE']], how='left', left_on='Số PO', right_on='MA_DON_HANG')
+        template_excel = template_excel.rename(columns={'DELIVERY_DATE': 'Ngày phân phối'})
+        template_excel.drop(['MA_DON_HANG'], axis=1, inplace=True)
+        template_excel = template_excel[['Số PO', 'STT', 'Ngày phân phối', 'Mã SP 7 số', 'Mã SP 13 số', 'Tên sản phẩm', 'Số lượng', 'Đơn giá', '% Thuế', 'Giá trị sau VAT', 'Hạn sử dụng tối thiểu']]
+        print('Created template excel - done')
+        flag = True
+    return template_excel, flag
 
 
 #17.03.2026
@@ -165,15 +182,11 @@ def func_main_process(input_list):
 '''Tùy chỉnh theo streamlit_17.03.2026'''
 # def func_execute(list_pdf):
 #     # list_pdf lúc này là danh sách các file object từ Streamlit
-#     # Hàm func_main_process bên dưới của bạn cần đảm bảo đọc được file object
 #     final_template = func_main_process(list_pdf)
-    
-#     # Thay vì lưu file .to_excel tại đây, ta chỉ trả về kết quả cho Streamlit hiện lên web
 #     if final_template is not None:
 #         print('Processed data - done')
 #         return final_template
     
-#     return None
 
 def func_execute(list_pdf):
     #danh sách các UploadedFile từ Streamlit
